96681bb4449bdad090c91caa899e8ff214bbd35c
MichaelBone <Michael.Bone@senet.com.au>
2018-12-02 14:19:03 +1030

    Changed to obtain two months of development applications.

--- scraper.ts
+++ scraper.ts
@@ -23,6 +23,7 @@ declare const process: any;
 // Address information.
 
 let SuburbNames = null;
+let HundredNames = null;
 
 // Sets up an sqlite database.
 
@@ -57,9 +58,9 @@ async function insertRow(database, developmentApplication) {
                 reject(error);
             } else {
                 if (this.changes > 0)
-                    console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and description \"${developmentApplication.description}\" into the database.`);
+                    console.log(`    Inserted: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\", description \"${developmentApplication.description}\" and received date \"${developmentApplication.receivedDate}\" into the database.`);
                 else
-                    console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\" and description \"${developmentApplication.description}\" because it was already present in the database.`);
+                    console.log(`    Skipped: application \"${developmentApplication.applicationNumber}\" with address \"${developmentApplication.address}\", description \"${developmentApplication.description}\" and received date \"${developmentApplication.receivedDate}\" because it was already present in the database.`);
                 sqlStatement.finalize();  // releases any locks
                 resolve(row);
             }
@@ -79,25 +80,37 @@ function sleep(milliseconds: number) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-// Format the address, ensuring that it has a valid suburb, state and post code.
+// Format the address, ensuring that it has a valid suburb name, state and post code.
 
 function formatAddress(address: string) {
-    address = address.trim();
+    // Remove a dot at the start of the address such as ". HD CLINTON" or in the middle of an
+    // address.  Remove duplicate spaces.  Remove any hundred name in brackets (that often
+    // appears after the suburb name).
+
+    address = address.replace(/^\. /g, " ").replace(/ \. /g, " ").replace(/ \(Hd.*?\)/gi, "").replace(/\s\s+/g, " ").trim();
     if (address === "")
-        return "";
+        return address;
 
-    // Remove any hundred name in brackets (that often appears after the suburb name).
+    // Do not attempt to format the address if it ends in a hundred name.  Otherwise the hundred
+    // name may be incorrectly interpreted as a suburb name.  For example, "HD CLINTON"
 
-    address = address.replace(/ \(Hd.*?\)/gi, "");
+    let uppercaseAddress = address.toUpperCase();
+    for (let hundredName of HundredNames) {
+        if (uppercaseAddress.toUpperCase() === "HD " + hundredName || uppercaseAddress.endsWith(" HD " + hundredName)) {
+            console.log(`The state and post code will not be added because the address ends with a hundred name: ${address}`);
+            return address;
+        }
+    }
 
     // Pop tokens from the end of the array until a valid suburb name is encountered (allowing
-    // for a few spelling errors).
+    // for a spelling error).  Prefer a longer suburb name over a shorter suburb name.  For
+    // example, prefer "PORT CLINTON" over "CLINTON".
 
     let tokens = address.split(" ");
 
     let suburbName = null;
-    for (let index = 1; index <= 4; index++) {
-        let suburbNameMatch = didyoumean(tokens.slice(-index).join(" "), Object.keys(SuburbNames), { caseSensitive: false, returnType: "first-closest-match", thresholdType: "edit-distance", threshold: 2, trimSpace: true });
+    for (let index = 4; index >= 1; index--) {
+        let suburbNameMatch = didyoumean(tokens.slice(-index).join(" "), Object.keys(SuburbNames), { caseSensitive: false, returnType: "first-closest-match", thresholdType: "edit-distance", threshold: 1, trimSpace: true });
         if (suburbNameMatch !== null) {
             suburbName = SuburbNames[suburbNameMatch];
             tokens.splice(-index, index);  // remove elements from the end of the array           
@@ -116,31 +129,19 @@ function formatAddress(address: string) {
     return (streetName + ((streetName === "") ? "" : ", ") + suburbName).trim();
 }
 
-// Parses the development applications.
+// Parses the development applications in the specified date range.
 
-async function main() {
-    // Read the suburb names.
+async function parse(dateFrom: moment.Moment, dateTo: moment.Moment, database) {
+    let dateFromText = encodeURIComponent(dateFrom.format("DD/MM/YYYY"));
+    let dateToText = encodeURIComponent(dateTo.format("DD/MM/YYYY"));
 
-    SuburbNames = {};
-    for (let line of fs.readFileSync("suburbnames.txt").toString().replace(/\r/g, "").trim().split("\n")) {
-        let suburbTokens = line.toUpperCase().split(",");
-        SuburbNames[suburbTokens[0].toUpperCase().trim()] = suburbTokens[1].toUpperCase().trim();
-    }
-
-    // Ensure that the database exists.
-
-    let database = await initializeDatabase();
-
-    // Retrieve the paged results of a search for the last month.
+    console.log(`Retrieving development applications from ${dateFrom.format("YYYY-MM-DD")} to ${dateTo.format("YYYY-MM-DD")}.`);
 
     let pageNumber = 0;
-
-    while (pageNumber++ < 50) {  // safety precaution
-        let dateFrom = encodeURIComponent(moment().subtract(1, "months").format("DD/MM/YYYY"));
-        let dateTo = encodeURIComponent(moment().format("DD/MM/YYYY"));
-        let developmentApplicationsUrl = DevelopmentApplicationsUrl.replace(/\{0\}/g, pageNumber.toString()).replace(/\{1\}/g, dateFrom).replace(/\{2\}/g, dateTo);
-
+    while (pageNumber++ < 100) {  // safety precaution
+        let developmentApplicationsUrl = DevelopmentApplicationsUrl.replace(/\{0\}/g, pageNumber.toString()).replace(/\{1\}/g, dateFromText).replace(/\{2\}/g, dateToText);
         console.log(`Retrieving page ${pageNumber}: ${developmentApplicationsUrl}`);
+
         let body = await request({ url: developmentApplicationsUrl, rejectUnauthorized: false, proxy: process.env.MORPH_PROXY });
         await sleep(2000 + getRandom(0, 5) * 1000);
         let $ = cheerio.load(body);
@@ -148,13 +149,13 @@ async function main() {
         // Parse the search results.
 
         for (let trElement of $("table.gv-table-view tr").get()) {
-            let url = $(trElement).find("#gv-field-31-1 a").attr("href");
-            if (url === undefined)
+            let developmentApplicationUrl = $(trElement).find("#gv-field-31-1 a").attr("href");
+            if (developmentApplicationUrl === undefined)
                 continue;
 
             // Obtain the description for the application.
 
-            let childBody = await request({ url: url, rejectUnauthorized: false, proxy: process.env.MORPH_PROXY });
+            let childBody = await request({ url: developmentApplicationUrl, rejectUnauthorized: false, proxy: process.env.MORPH_PROXY });
             let childPage = cheerio.load(childBody);
 
             let address = formatAddress($(trElement).find("#gv-field-31-7").text().trim());
@@ -180,7 +181,7 @@ async function main() {
                     applicationNumber: applicationNumber,
                     address: address,
                     description: description,
-                    informationUrl: DevelopmentApplicationsUrl,
+                    informationUrl: developmentApplicationUrl,
                     commentUrl: CommentUrl,
                     scrapeDate: moment().format("YYYY-MM-DD"),
                     receivedDate: receivedDate.isValid ? receivedDate.format("YYYY-MM-DD") : ""
@@ -191,9 +192,48 @@ async function main() {
         // If there is no "next page" link then assume this is the last page.
 
         let hasNextPageLink = ($("ul.page-numbers li a.next").length > 0);
-        if (!hasNextPageLink)
-            break;
+        if (!hasNextPageLink) {
+            console.log("Reached the last page of the paged search results.");
+            return;
+        }
     }
+
+    console.log(`Stopped because reached ${pageNumber} pages.`);
+}
+
+// Parses the development applications.
+
+async function main() {
+    // Read the suburb names.
+
+    SuburbNames = {};
+    for (let line of fs.readFileSync("suburbnames.txt").toString().replace(/\r/g, "").trim().split("\n")) {
+        let suburbTokens = line.toUpperCase().split(",");
+        SuburbNames[suburbTokens[0].toUpperCase().trim()] = suburbTokens[1].toUpperCase().trim();
+    }
+
+    // Read the hundred names.
+
+    HundredNames = [];
+    for (let hundredName of fs.readFileSync("hundrednames.txt").toString().replace(/\r/g, "").trim().split("\n"))
+        HundredNames.push(hundredName.toUpperCase().trim());
+
+    // Ensure that the database exists.
+
+    let database = await initializeDatabase();
+
+    // Obtain the paged results of a search for the last month.
+
+    await parse(moment().subtract(1, <moment.unitOfTime.DurationConstructor> "months"), moment(), database);
+    await sleep(5000 + getRandom(0, 10) * 1000);
+
+    // Obtain the paged results of a search for a randomly selected month (to build up over time
+    // a complete picture of all development applications, while avoiding overloading the web
+    // server with a lot of requests).
+
+    let monthCount = moment().year() * 12 + moment().month() - (1997 * 12 + 4);  // first recorded development application is 16th April 1997
+    let randomMonth = getRandom(1, monthCount + 1)
+    await parse(moment().subtract(randomMonth + 1, <moment.unitOfTime.DurationConstructor> "months"), moment().subtract(randomMonth, <moment.unitOfTime.DurationConstructor> "months"), database);
 }
 
 main().then(() => console.log("Complete.")).catch(error => console.error(error));
